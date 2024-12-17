0f1f30647d6d39a23bc55b5e73e9ea84aa3cdeea
zitro <ajosueramos@gmail.com>
2024-12-17 00:42:44 -0500

    Proyecto terminado

--- src/routes/persona.routes.js
+++ src/routes/persona.routes.js
@@ -7,10 +7,57 @@ router.get('/add', (req,res)=>{
     res.render('personas/add');
 });
 
+router.post('/add', async(req, res)=>{
+    try {
+        const {name, lastname, age} = req.body;
+        const newPersona = {
+            name, lastname, age
+        }
+        await pool.query('INSERT INTO Personas Set ?', [newPersona])
+        res.redirect('list');
+    } catch (error) {
+        res.status(500).json({message:err.message});
+    }
+})
+
 router.get('/list', async(req, res)=>{
     try {
         const [result] = await pool.query('SELECT * FROM Personas');
         res.render('personas/list', {personas: result});
+    } catch (error) {
+        res.status(500).json({message:err.message});
+    }
+})
+
+router.get('/edit/:id', async (req, res)=>{
+    try {
+        const {id} = req.params;
+        const [persona] = await pool.query("SELECT * FROM Personas WHERE id = ?", [id]);
+        const personaEdit = persona[0];
+        res.render('personas/edit', {persona: personaEdit});
+
+    } catch (error) {
+        res.status(500).json({message:err.message});
+    }
+})
+
+router.post('/edit/:id', async(req, res)=>{
+    try {
+        const {name, lastname, age} = req.body;
+        const {id} = req.params;
+        const editPersona = {name, lastname, age};
+        await pool.query('UPDATE Personas Set ? Where id = ?', [editPersona, id])
+        res.redirect('/list');
+    } catch (error) {
+        res.status(500).json({message:err.message});
+    }
+})
+
+router.get('/delete/:id', async(req, res)=>{
+    try {
+        const {id} = req.params;
+        await pool.query("Delete From Personas Where id = ?", [id])
+        res.redirect("/list")
     } catch (error) {
         res.status(500).json({message:err.message});
     }
